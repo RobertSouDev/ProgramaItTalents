6f94493cb3886d79eadcac203460b1172c6f6b20
RobertSouDev <rogerhallks1@gmail.com>
2024-02-05 23:58:08 -0300

    convertendo str

---
+++ src/exercicios/ex0003/ConversaoDeStr.java
@@ -0,0 +1,10 @@
+package exercicios.ex0003;
+
+public class ConversaoDeStr {
+    public static void main(String[] args) {
+        String valor = "10";
+
+        int valorComFrente = Integer.parseInt(valor) + 2;
+        System.out.println(valorComFrente);
+    }
+}
